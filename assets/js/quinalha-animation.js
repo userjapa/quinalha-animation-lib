6f0b1971df7696ee3fdb7bd930a12cd497149b6f
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-12-09 02:16:45 -0200

    getting class from dataset

--- assets/js/quinalha-animation.js
+++ assets/js/quinalha-animation.js
@@ -1,20 +1,21 @@
+const animations = document.getElementsByClassName('animation')
+
 const showing = el => {
-  var windowTop = window.scrollY
-  var windowBottom = windowTop + window.innerHeight
-  var elemTop = el.offsetTop
-  var elemBottom = elemTop + el.offsetHeight
+  const windowTop = window.scrollY
+  const windowBottom = windowTop + window.innerHeight
+  const elemTop = el.offsetTop
+  const elemBottom = elemTop + el.offsetHeight
   return ((elemBottom <= windowBottom) && (elemTop >= windowTop))
 }
 
 window.onscroll = () => {
-  const animations = document.getElementsByClassName('animation')
   for (x of animations) {
     if (showing(x)) {
-      x.classList.remove('showOut')
-      x.classList.add('showIn')
+      x.classList.remove(x.dataset.leaveAnimate)
+      x.classList.add(x.dataset.enterAnimate)
     } else {
-      x.classList.remove('showIn')
-      x.classList.add('showOut')
+      x.classList.remove(x.dataset.enterAnimate)
+      x.classList.add(x.dataset.leaveAnimate)
     }
     console.log(x.classList)
   }
