c6c07e00a80c850de65e700a995349e0843ba789
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-12-10 15:23:31 -0200

    changing trasition

--- assets/js/quinalha-animation.js
+++ assets/js/quinalha-animation.js
@@ -3,14 +3,14 @@ const animations = document.getElementsByClassName('animation')
 const showing = el => {
   const windowTop = window.scrollY
   const windowBottom = windowTop + window.innerHeight
-  const elemTop = el.offsetTop
-  const elemBottom = elemTop + el.offsetHeight
-  return ((elemBottom <= windowBottom) && (elemTop >= windowTop))
+  let elemTop = el.offsetTop
+  let elemBottom = elemTop + el.offsetHeight
+  return ((elemBottom >= windowBottom) || (elemBottom <= windowTop)) && ((elemTop <= windowTop) || (elemTop >= windowBottom))
 }
 
 window.onscroll = () => {
   for (x of animations) {
-    if (showing(x)) {
+    if (!showing(x)) {
       x.classList.remove(x.dataset.leaveAnimate)
       x.classList.add(x.dataset.enterAnimate)
     } else {
